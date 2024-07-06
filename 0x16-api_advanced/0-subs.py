4715da7e6dfff75a069ae41929e484be228c116d
akramhany <akramhany65@gmail.com>
2024-07-06 17:43:40 +0300

    fix: another fix for tasak 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,15 +14,13 @@ BASE_URL = 'http://www.reddit.com/'
 def number_of_subscribers(subreddit):
     "takes a subreddit name, returns it's subs count"
 
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
     api_url = 'r/' + subreddit + '/about.json'
-    headers = {'User-Agent': 'getNumOfSubs by Akram',
-               'Accept': 'application/json'}
+    headers = {'User-Agent': 'getNumOfSubs by Akram'}
 
     r = requests.get(BASE_URL + api_url, headers=headers)
 
-    try:
-        subscribers_count = r.json()['data']['subscribers']
-    except Exception:
-        subscribers_count = 0
-
+    subscribers_count = r.json().get('data', {}).get('subscribers', 0)
     return subscribers_count
