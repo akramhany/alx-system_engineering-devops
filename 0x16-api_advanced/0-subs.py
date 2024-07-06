c57c0d9ece910a5ac65acc37af4cc29c8a8a59c1
akramhany <akramhany65@gmail.com>
2024-07-06 17:47:38 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,19 +8,17 @@ subreddit.
 import requests
 
 
-BASE_URL = 'http://www.reddit.com/'
-
-
 def number_of_subscribers(subreddit):
     "takes a subreddit name, returns it's subs count"
 
     if subreddit is None or type(subreddit) is not str:
         return 0
 
-    api_url = 'r/' + subreddit + '/about.json'
     headers = {'User-Agent': 'getNumOfSubs by Akram'}
 
-    r = requests.get(BASE_URL + api_url, headers=headers)
+    r = requests.get(
+        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers=headers)
 
     subscribers_count = r.json().get('data', {}).get('subscribers', 0)
     return subscribers_count
