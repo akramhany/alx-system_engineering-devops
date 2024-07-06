835c45ebf3f95c0f0cee5fde002d1722d2c996d7
akramhany <akramhany65@gmail.com>
2024-07-06 18:30:39 +0300

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,15 @@ def top_ten(subreddit):
     """prints the titles of the first 10 hottest posts liked for a
     given subreddit"""
 
-    r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+    r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(subreddit),
                      headers={'User-Agent': 'Myscript for 10 hottest posts'},
-                     params={'limit': 10})
+                     params={'limit': 10}, allow_redirects=False)
+
+    if r.status_code >= 300:
+        print('None')
+        return
 
     posts = r.json().get('data', {}).get('children', [])
 
-    if not posts:
-        print('None')
     for post in posts:
         print(post.get('data', {}).get('title'))
