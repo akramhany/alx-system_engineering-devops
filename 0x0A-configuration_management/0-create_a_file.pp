6fe739d6e4223b31109f4aa0040906be4b440964
akramhany <akramhany65@gmail.com>
2024-07-08 21:50:03 +0300

    feat: write a script that creates the school file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a new file in /tmp and configure it
+
+file { 'create_school_file':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
